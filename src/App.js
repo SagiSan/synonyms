bd612aabe3219fdf470739dc6fc8bd7b9d4e0aea
SagiSan <samir.sagi@gmail.com>
2019-10-29 17:14:39 +0100

    add search

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ class App extends Component {
       word: '',
       synonym: '',
       synonyms_list: [],
-      words: []
+      words: {},
+      search: '',
+      search_results: []
     }
   }
   addWord = () => {
-    let index = this.state.words.indexOf(this.state.word);
-    let words = [...this.state.words];
-    if (index > -1) {
-      words[index].synonyms = [...this.state.synonyms_list];
+    let words = { ...this.state.words };
+    if (words[this.state.word]) {
+      words[this.state.word].synonyms = [...this.state.synonyms_list];
       this.setState({ words }, () => console.log(this.state.words));
     } else {
-      words.push({
+      words[this.state.word] = {
         name: this.state.word,
         synonyms: [...this.state.synonyms_list]
-      });
+      };
       this.setState({ words }, () => console.log(this.state.words));
     }
+    this.setState({ synonyms_list: [], word: '' });
   }
   updateSynonyms = key => {
     if (key === 'add') {
@@ -32,22 +34,44 @@ class App extends Component {
       console.log('find and update synonym');
     }
   }
+  onChangeWord = e => {
+
+    if (this.state.words[e.target.value]) {
+      this.setState({ synonyms_list: [...this.state.words[e.target.value].synonyms] })
+    }
+    this.setState({ word: e.target.value });
+  }
+  search = e => {
+    let value = e.target.value;
+    let synonyms = [];
+    if (this.state.words[value]) {
+      synonyms = [...this.state.words[value].synonyms];
+    }
+    this.setState({ search: value, search_results: [...synonyms] });
+  }
   render() {
-    const { word, synonym, synonyms_list } = this.state;
+    const { word, synonym, synonyms_list, search, search_results } = this.state;
     const synonyms = <ul>
       {synonyms_list.length > 0 && synonyms_list.map(syn => {
         return (
           <li key={syn}>{syn}</li>
         )
       })}
-    </ul>
+    </ul>;
+    const search_synonyms = <ul>
+      {search_results.length > 0 && search_results.map((syn, i) => {
+        return (
+          <li key={`${syn}_${i}`}>{syn}</li>
+        )
+      })}
+    </ul>;
     return (
       <div className="App">
         <header>
           <h1>Synonym Dictionary</h1>
         </header>
         <main>
-          <input type='text' placeholder="Enter your word" value={word} onChange={e => this.setState({ word: e.target.value, synonyms_list: [] })} />
+          <input type='text' placeholder="Enter your word" value={word} onChange={this.onChangeWord} />
           <br />
           {synonyms_list.length > 0 && synonyms}
           <br />
@@ -55,8 +79,15 @@ class App extends Component {
           {word.length > 0 && <button disabled={!synonyms_list.indexOf(synonym)} onClick={() => this.updateSynonyms('add')}>Add</button>}
           <br />
           <br />
-          {word.length > 0 && <button onClick={this.addWord}>Add word</button>}
+          {word.length > 0 && synonyms_list.length > 0 && <button onClick={this.addWord}>Save word</button>}
         </main>
+        <section className='search'>
+          <hr />
+          <h2>Search words</h2>
+          <input type='text' placeholder="search" value={search} onChange={this.search} />
+          <br />
+          {search_synonyms}
+        </section>
       </div>
     );
   }
