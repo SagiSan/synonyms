1a1ff0db86bfeeb56b8b41df12b7323fffee91bd
SagiSan <samir.sagi@gmail.com>
2019-10-30 22:00:38 +0100

    Updates

---
+++ precache-manifest.fd91d080a4c186215b1aa0a0849606be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d7b98649a2a9f0c6bc14c331d1c58d",
+    "url": "/SagiSan/synonyms/index.html"
+  },
+  {
+    "revision": "c1c154eda6c94ec0d288",
+    "url": "/SagiSan/synonyms/static/css/main.f8d34488.chunk.css"
+  },
+  {
+    "revision": "e0f68eb39afe8e8fe5c0",
+    "url": "/SagiSan/synonyms/static/js/2.bdef7236.chunk.js"
+  },
+  {
+    "revision": "c1c154eda6c94ec0d288",
+    "url": "/SagiSan/synonyms/static/js/main.a7666be9.chunk.js"
+  },
+  {
+    "revision": "1ae8101919d1de03e9b6",
+    "url": "/SagiSan/synonyms/static/js/runtime-main.4cfb712f.js"
+  }
+]);
